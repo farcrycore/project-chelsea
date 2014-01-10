6770bd293d3d8c964fce2893a71bd338bdc27bb1
Geoff Bowers <modius@daemon.com.au>
2014-01-10 23:16:34 +1100

    Update project data for installer.

--- install/INSERT-ruleText-1.sql
+++ install/INSERT-ruleText-1.sql
@@ -1,7 +1,7 @@
-INSERT INTO ruleText (datetimelastupdated,text,objectID,label,title) VALUES ( '2011:12:16 12:28:33','<h3>Build amazing web applications with <span>FarCry 6.1</span></h3>
+INSERT INTO ruleText (datetimelastupdated,text,objectID,label,title) VALUES ( '2011-12-16 12:28:33','<h3>Build amazing web applications with <span>FarCry 6.1</span></h3>
 <a class="button-intro" href="http://www.farcrycore.org/download">Download Now <span>(6.1.1)</span></a>','44858440-2785-11E1-A2FB001C42000009','Home Page Intro','' );
-INSERT INTO ruleText (datetimelastupdated,text,objectID,label,title) VALUES ( '2011:12:15 23:20:56','<h3>Build amazing web applications with <span>FarCry 6.1</span></h3>
+INSERT INTO ruleText (datetimelastupdated,text,objectID,label,title) VALUES ( '2011-12-15 23:20:56','<h3>Build amazing web applications with <span>FarCry 6.1</span></h3>
 <a class="button-intro" href="http://www.farcrycore.org/download">Download Now <span>(6.1.1)</span></a>','40775900-2717-11E1-93B860C547035CDE','Home Page Intro','' );
-INSERT INTO ruleText (datetimelastupdated,text,objectID,label,title) VALUES ( '2011:11:11 17:40:49','<h3>Build amazing web applications with <span>FarCry 6.2</span></h3>
+INSERT INTO ruleText (datetimelastupdated,text,objectID,label,title) VALUES ( '2011-11-11 17:40:49','<h3>Build amazing web applications with <span>FarCry 6.2</span></h3>
 <a class="button-intro" href="/">Download Now <span>(6.2.0)</span></a>','17D034B0-0C30-11E1-8CC5001C42000009','Home Page Intro','' );
 
